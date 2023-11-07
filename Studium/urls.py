9bc00b77c6a94593ee11187a9c83b1904e76fa0d
Funiek01 <116259754+Funiek01@users.noreply.github.com>
2023-11-07 19:54:31 +0100

    Most UI changes

--- Studium/urls.py
+++ Studium/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('room/', views.room, name="room")
+    path('room/', views.room, name="room"),
+    path('quizzes/', views.quizzes, name='quizzes'),
+    path('quiz/', views.quiz, name='quiz'),
+    path('flashcards/', views.flashcards, name='flashcards'),
+    path('flashcard/', views.flashcard, name='flashcard'),
+    path('choose-learning/', views.learning_choice, name='learning-choice'),
+    path('my-progress/', views.my_progress, name='my-progress'),
+    path('my-result/', views.my_result, name='my-result'),
 ]
